b2bc3d65c8da0b977eb4f25ef71577351e0cae16
Josh Wainwright <wainwright.ja@gmail.com>
2013-04-07 20:56:00 +0100

    Added some plugins from oh-my-zsh

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -10,3 +10,7 @@ alias rest='cd ~/.scripts/st; makepkg -g >> PKGBUILD; makepkg -fi --noconfirm'
 alias g++='g++ -Wall -o'
 
 alias useless='while [ true ]; do head -n 100 /dev/urandom; sleep .1; done | hexdump -C | grep "ca fe"'
+
+paclist() {
+  sudo pacman -Qei $(pacman -Qu|cut -d" " -f 1)|awk ' BEGIN {FS=":"}/^Name/{printf("\033[1;36m%s\033[1;37m", $2)}/^Description/{print $2}'
+}
