348c82d7bf56cf5fda432a4adae15c22e4fc8697
josh wainwright <wainwright.ja@gmail.com>
2013-04-07 16:09:18 +0100

    Added zsh config, split between contents of zsh folder

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,12 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
+alias ls='ls --color=auto'
+alias cp="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
+alias mv='mv -i -v'
+alias less='less -F'
+alias sprunge='curl -F "sprunge=<-" http://sprunge.us'
+alias y3p='cd ~/Documents/Physics/Year\ 3'
+alias redwm='cd ~/.scripts/dwm; makepkg -g >> PKGBUILD; makepkg -fi --noconfirm; killall dwm'
+alias rest='cd ~/.scripts/st; makepkg -g >> PKGBUILD; makepkg -fi --noconfirm'
+
+alias g++='g++ -Wall -o'
+
+alias useless='while [ true ]; do head -n 100 /dev/urandom; sleep .1; done | hexdump -C | grep "ca fe"'
