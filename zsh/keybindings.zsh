f6672d3eb1c3fc07f22494de992e8e44d08165eb
Josh Wainwright <wainwright.ja@gmail.com>
2013-04-08 10:25:44 +0100

    Trying to get rid of compdef error

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -5,3 +5,11 @@ bindkey    "^[3;5~"         delete-char
 #Allow forward and backward by word #
 bindkey ';5D' emacs-backward-word
 bindkey ';5C' emacs-forward-word
+
+bindkey '^[^[[D' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[[5D' beginning-of-line
+bindkey '^[[5C' end-of-line
+bindkey '^[[3~' delete-char
+bindkey '^[^N' newtab
+bindkey '^?' backward-delete-char
