664473f09d7d41cf7554e8f2ba303fcd1e1ee3a8
Josh Wainwright <wainwright.ja@gmail.com>
2013-05-23 15:42:48 +0100

    Restructuring of files to a more program centered model

--- media/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias mplayer='mplayer -msgcolor -nolirc -nojoystick'
-alias mute-beep='xset -b && sudo rmmod pcspkr'
-alias play-dvd='mplayer -nocache -dvd-device /dev/sr0 -mouse-movements dvdnav://'
--- system/env.zsh
+++
@@ -1,2 +0,0 @@
-#export EDITOR='subl'
-export EDITOR='vim'
