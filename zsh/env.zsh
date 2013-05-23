4c30168fedb9d778265e6faa9ff187e413a0e549
Josh Wainwright <wainwright.ja@gmail.com>
2013-05-23 15:43:46 +0100

    Restructing of files to a more program centered model

---
+++ zsh/env.zsh
@@ -0,0 +1,9 @@
+#export EDITOR='subl'
+export EDITOR='vim'
+export MANPAGER=less
+
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export VMAIL_BROWSER='luakit'
