3a107e18ea8f41e5dc1669582d856ca6feab988a
Josh Wainwright <wainwright.ja@gmail.com>
2013-06-01 12:11:43 +0100

    Misc config edits

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -33,3 +33,5 @@ alias mplayer='mplayer -msgcolor -nolirc -nojoystick'
 alias mute-beep='xset -b && sudo rmmod pcspkr'
 alias play-dvd='mplayer -nocache -dvd-device /dev/sr0 -mouse-movements dvdnav://'
 
+
+alias vless='vim -u /usr/share/vim/vim73/macros/less.vim'  
