f650a44405a08e7071e4ebcd8435350008653046
Josh Wainwright <wainwright.ja@gmail.com>
2013-11-05 08:30:00 +0000

    Add tprot email formatter

--- email/mutt.symlink/autoAlias.sh
+++ email/mutt.symlink/autoAlias.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#MESSAGE=$(cat | par -qefigd)
 MESSAGE=$(cat)
 
 NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk '{$1=""; if (NF == 3) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
